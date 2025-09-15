590cd70ff969ab75f13825a090f36d5ac016fa3a
Wei Cheng <54185148+chengwei0815@users.noreply.github.com>
2025-09-15 14:42:41 -0400

    Add files via upload

---
+++ C# HW1/DataTypeInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Coding：
+1.How can we find the minimum and maximum values, as well as the number of bytes, for the following data types: sbyte, byte, short, ushort, int, uint, long, ulong, float, double, and decimal?
+
+*/
+namespace HW1
+{
+    public class DataTypeInfo
+    {
+        public static void DataType()
+        {
+          
+            Console.WriteLine("sbyte: " + sbyte.MinValue + " " + sbyte.MaxValue + " " + sizeof(sbyte));
+            Console.WriteLine("byte: " + byte.MinValue + " " + byte.MaxValue + " " + sizeof(byte));
+            Console.WriteLine("short: " + short.MinValue + " " + short.MaxValue + " " + sizeof(short));
+            Console.WriteLine("ushort: " + ushort.MinValue + " " + ushort.MaxValue + " " + sizeof(ushort)); 
+            Console.WriteLine("int: " + int.MinValue + " " + int.MaxValue + " " + sizeof(int)); 
+            Console.WriteLine("uint: " + uint.MinValue + " " + uint.MaxValue + " " + sizeof(uint)); 
+            Console.WriteLine("long: " + long.MinValue + " " + long.MaxValue + " " + sizeof(long)); 
+            Console.WriteLine("ulong: " + ulong.MinValue + " " + ulong.MaxValue + " " + sizeof(ulong)); 
+            Console.WriteLine("float: " + float.MinValue + " " + float.MaxValue + " " + sizeof(float));
+            Console.WriteLine("double: " + double.MinValue + " " + double.MaxValue + " " + sizeof(double));
+            Console.WriteLine("decimal: " + decimal.MinValue + " " + decimal.MaxValue + " " + sizeof(decimal));
+         
+        }
+    }
+}
---
+++ C# HW1/LoopOverflow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 3. What will happen if this code executes?
+int max = 500;
+for (byte i = 0; i < max; i++)
+{
+    Console.WriteLine(i);
+}
+
+ */
+namespace HW1
+{
+    public class LoopOverflow
+    {
+        public static void Run()
+        {
+            int max = 500;
+            for (byte i = 0; i < max; i++) // will overflow
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+// The code will result in an infinite loop because the byte variable 'i' will overflow after reaching its maximum value of 255, wrapping around to 0 and continuing the loop indefinitely.
+// To fix this, change the type of 'i' from byte to int:
+// for (int i = 0; i < max; i++)
