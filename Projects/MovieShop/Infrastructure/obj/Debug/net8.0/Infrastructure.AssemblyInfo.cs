9d2a659c662cada7f34854b3d8c16f01c589b57d
chengwei0815 <chengwei0815@gmail.com>
2025-10-12 13:29:17 -0400

    Added Pagination to UI

--- Projects/MovieShop/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Projects/MovieShop/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf07366b096cf828301595bda788d05dff324d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe7995246109ef9ee12cdd433c3a0f1e2fa45b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
