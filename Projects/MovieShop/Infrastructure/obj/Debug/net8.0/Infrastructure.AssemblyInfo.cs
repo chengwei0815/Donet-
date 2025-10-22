688fb968b2e3497999416e3062088db961596c17
chengwei0815 <chengwei0815@gmail.com>
2025-10-22 17:44:22 -0400

    Added JWT

--- Projects/MovieShop/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Projects/MovieShop/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7c6514a2fdb685417e644d96070af85a5c0cf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6751724eaa6ec5e61ac3d301f682154e455425f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
